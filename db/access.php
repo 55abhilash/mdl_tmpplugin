d9fa840466e29e8b9fdebeceb2604d664def758e
Abhilash Mhaisne <55abhilash@openmailbox.org>
2016-03-02 09:50:30 +0530

    Removed all files which are currently not necessary. Will add one by one. Most of the plugins do not have all these files.

---
+++ db/access.php
@@ -0,0 +1,8 @@
+<?php
+
+$local_mdl_plugin_capabilities = array(
+	'local/mdl_plugin:tmpcap' => array(
+		'captype' = 'read',
+		'contextlevel' = CONTEXT_SYSTEM
+	),
+);
--- settings.php
+++ settings.php
@@ -2,6 +2,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-if(is_siteadmin()) {
-	$ADMIN->add('themes', new admin_category('local_mdl_tmpplugin', 'mdl_tmpplugin'));
+if($hassiteconfig) {
+	$settings = new admin_settingpage('local_mdl_tmplugin', 'mdl_tmpplugin');
+	$ADMIN->add('localplugins', $settings);
+	$settings->add(new admin_setting_configtext('local_mdl_plugin/option', 'Info', 'This is a temporary moodle plugin', 100, PARAM_INT));
 }
