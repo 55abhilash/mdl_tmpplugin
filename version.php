34f6cd6c9333b715caeb1f5ccfb1a0a6ac65042a
root <root@localhost.localdomain>
2016-03-02 14:04:10 +0530

    First commit

---
+++ version.php
@@ -0,0 +1,7 @@
+<?php
+$plugin->component = "local_tmpplugin";
+$plugin->version = 2016022200; // Date on which this version of plugin is released. YYYYMMDDVN : VN = 00 - version 1, 11 - version 2.....
+$plugin->requires = 2015111600; //The min. moodle version required
+$plugin->maturity = MATURITY_ALPHA; //Site can be configured by admins to get updates for plugins with only higher maturity levels
+$plugin->release = 'v3.0-r1'; //Specify a human readable string for the release no. of  plugin
+//$plugin->dependencies = ;
